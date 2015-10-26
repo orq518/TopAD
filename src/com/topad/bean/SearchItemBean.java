8f47564dee84f6f216ae06eeb56a17289fc847e9
欧瑞强 <ouruiqiang@ucfgroup.com>
2015-10-26 23:13:34 +0800

    优化搜搜页面

---
+++ src/com/topad/bean/SearchItemBean.java
@@ -0,0 +1,19 @@
+package com.topad.bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ouruiqiang
+ * @data: on 15/10/20 10:11
+ */
+public class SearchItemBean implements Serializable {
+    /**
+     * serialVersionUID:TODO（用一句话描述这个变量表示什么）
+     */
+    private static final long serialVersionUID = 4768927122317982665L;
+    public String locaion;
+    public String name;
+    public String type;
+    public String voice;
+}
